9b6d281c5144b631f8e021d794dafbceb7b20513
ladys <lady.shkurnaya@gmail.com>
2025-09-29 20:20:27 +0300

    Add logger

--- ConferenceWebApp/Program.cs
+++ ConferenceWebApp/Program.cs
@@ -1,4 +1,4 @@
-using ConferenceWebApp.Application.Extensions;
+﻿using ConferenceWebApp.Application.Extensions;
 using ConferenceWebApp.Domain.Entities;
 using ConferenceWebApp.Infrastructure.Extensions;
 using ConferenceWebApp.Persistence;
@@ -6,6 +6,7 @@ using ConferenceWebApp.Persistence.Extensions;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Session;
+using Serilog;
 
 namespace ConferenceWebApp;
 
@@ -13,122 +14,144 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build())
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.File("Logs/log-.txt",
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 7,
+                shared: true)
+            .CreateLogger();
 
-
-        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-
-        builder.Services.AddProblemDetails();
-        var configBuild = new ConfigurationBuilder()
-            .SetBasePath(builder.Environment.ContentRootPath)
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddEnvironmentVariables();
-
-        var configuration = configBuild.Build();
-
-
-        builder.Services.AddHttpContextAccessor();
-
-        builder.Services.AddDistributedMemoryCache();
-
-     
-
-        builder.Services.AddDatabase(builder.Configuration);
-        builder.Services.AddApplicationServices();
-        builder.Services.AddPersistence();
-
-
-        builder.Services.AddFluentValidationAutoValidation(options =>
+        try
         {
-            options.DisableDataAnnotationsValidation = false;
-        });
-        builder.Services.AddValidators();
+            Log.Information(" Приложение запускается...");
 
-        var rootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
-        builder.Services.AddInfrastructure(rootPath);
+            var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddScoped<UserManager<User>, CustomUserManager>();
-        builder.Services.AddIdentity<User, IdentityRole<Guid>>(options =>
-        {
-            options.Password.RequireDigit = false;
-            options.Password.RequiredLength = 8;
-            options.Password.RequireNonAlphanumeric = false;
-            options.User.RequireUniqueEmail = true;
-            options.SignIn.RequireConfirmedEmail = true;
-            options.Tokens.ProviderMap.Add("Email", new TokenProviderDescriptor(typeof(EmailTokenProvider<User>)));
-            options.Tokens.EmailConfirmationTokenProvider = "Email";
-        })
-        .AddEntityFrameworkStores<AppDbContext>()
-        .AddDefaultTokenProviders();
+            // Подключаем Serilog вместо стандартного логгера
+            builder.Host.UseSerilog();
 
-        builder.Services.AddSession(options =>
-        {
-            options.IdleTimeout = TimeSpan.FromHours(20);
-            options.Cookie.HttpOnly = true;
-            options.Cookie.IsEssential = true;
-        });
+            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+            builder.Services.AddProblemDetails();
 
-        builder.Services.ConfigureApplicationCookie(options =>
-        {
-            options.LoginPath = "/Auth/Login";
-            options.LogoutPath = "/Auth/Logout";
-            options.AccessDeniedPath = "/Auth/AccessDenied";
-            options.ExpireTimeSpan = TimeSpan.FromHours(20);
-            options.SlidingExpiration = true;
-            options.Cookie.HttpOnly = true;
-            options.Cookie.SecurePolicy = builder.Environment.IsDevelopment()
-                ? CookieSecurePolicy.None
-                : CookieSecurePolicy.Always;
-        });
+            var configBuild = new ConfigurationBuilder()
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
 
-        builder.Services.AddControllersWithViews();
+            var configuration = configBuild.Build();
 
-        var app = builder.Build();
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddDistributedMemoryCache();
 
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
-        else
-        {
-            app.UseHsts();
-            app.UseExceptionHandler("/error");
-            app.UseStatusCodePagesWithReExecute("/error/{0}");
-        }
+            builder.Services.AddDatabase(builder.Configuration);
+            builder.Services.AddApplicationServices();
+            builder.Services.AddPersistence();
 
-
-        app.UseMiddleware<SessionMiddleware>();
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-        app.UseSession();
-        app.UseRouting();
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}"
-        );
-
-        using (var scope = app.Services.CreateScope())
-        {
-            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
-            var roles = new[] { "Participant", "Admin", "SuperAdmin" };
-
-            foreach (var role in roles)
+            builder.Services.AddFluentValidationAutoValidation(options =>
             {
-                if (!await roleManager.RoleExistsAsync(role))
+                options.DisableDataAnnotationsValidation = false;
+            });
+            builder.Services.AddValidators();
+
+            var rootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+            builder.Services.AddInfrastructure(rootPath);
+
+            builder.Services.AddScoped<UserManager<User>, CustomUserManager>();
+            builder.Services.AddIdentity<User, IdentityRole<Guid>>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = true;
+                options.Tokens.ProviderMap.Add("Email", new TokenProviderDescriptor(typeof(EmailTokenProvider<User>)));
+                options.Tokens.EmailConfirmationTokenProvider = "Email";
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromHours(20);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Auth/Login";
+                options.LogoutPath = "/Auth/Logout";
+                options.AccessDeniedPath = "/Auth/AccessDenied";
+                options.ExpireTimeSpan = TimeSpan.FromHours(20);
+                options.SlidingExpiration = true;
+                options.Cookie.HttpOnly = true;
+                options.Cookie.SecurePolicy = builder.Environment.IsDevelopment()
+                    ? CookieSecurePolicy.None
+                    : CookieSecurePolicy.Always;
+            });
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+                app.UseExceptionHandler("/error");
+                app.UseStatusCodePagesWithReExecute("/error/{0}");
+            }
+
+            app.UseMiddleware<SessionMiddleware>();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+                var roles = new[] { "Participant", "Admin", "SuperAdmin" };
+
+                foreach (var role in roles)
                 {
-                    await roleManager.CreateAsync(new IdentityRole<Guid>
+                    if (!await roleManager.RoleExistsAsync(role))
                     {
-                        Name = role,
-                        NormalizedName = role.ToUpper()
-                    });
+                        await roleManager.CreateAsync(new IdentityRole<Guid>
+                        {
+                            Name = role,
+                            NormalizedName = role.ToUpper()
+                        });
+                    }
                 }
             }
-        }
 
-        await app.RunAsync();
+            await app.RunAsync();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Приложение завершилось с фатальной ошибкой");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
